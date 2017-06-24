aaaed03f81fa39a1d0a90e589ff419a084194ad5
Márcio Reis <marcio.reis@outlook.com>
2017-06-24 12:02:38 +0200

    Add filtering by id to our graphql

--- index.js
+++ index.js
@@ -6,22 +6,38 @@ var http = require('http'),
 	buildSchema = require('graphql').buildSchema,
 	graphqlHTTP = require('express-graphql');
 
-const mySchema = buildSchema(`
-	type Video {
-		id: ID,
-		title: String,
-		duration: Int,
-		watched: Boolean
-	}
-	type Query {
-		video: Video,
-		videos: [Video]
-	}
+var GraphQLID = require('graphql').GraphQLID;
+var GraphQLSchema = require('graphql').GraphQLSchema;
+var GraphQLInt = require('graphql').GraphQLInt;
+var GraphQLString = require('graphql').GraphQLString;
+var GraphQLBoolean = require('graphql').GraphQLBoolean;
+var GraphQLObjectType = require('graphql').GraphQLObjectType;
 
-	type Schema {
-		query: Query
-	}
-`)
+/***
+{
+  video(id:"123") {
+    id,
+    title,
+    duration
+  }
+}
+
+will produce: 
+{
+  "data": {
+    "video": {
+      "id": "123",
+      "title": "Film A",
+      "duration": 1203
+    }
+  },
+  "extensions": {
+    "runTime": 4
+  }
+}
+*/
+
+
 
 
 const videoA = {
@@ -31,49 +47,80 @@ const videoA = {
 	title: 'Film A'
 }
 const videoB = {
-	id: 123,
+	id: 124,
 	watched: true,
 	duration: 1203,
 	title: 'Film B'
 }
+const videos = [videoA, videoB]
 
-const myResolvers = {
-	video: () => videoA,
-	videos: () => {
-		return new Promise(function(resolve){
-			setTimeout(function(){
-				resolve([videoA, videoB]);
-			},2000)
-		})
+const videoType = new GraphQLObjectType({
+	name: 'Video',
+	description: 'A video',
+	fields: {
+		id: {
+			type: GraphQLID,
+			description: 'The video id'
+		},
+		title: {
+			type: GraphQLString,
+			description: 'The video title'
+		},
+		duration: {
+			type: GraphQLInt,
+			description: 'The video duration'
+		},
+		watched: {
+			type: GraphQLBoolean,
+			description: 'The video watched state'
+		}
 	}
+})
+
+const queryType = new GraphQLObjectType({
+	name: 'QueryType',
+	description: 'The root query type.',
+	fields: {
+		video: {
+			type: videoType,
+			args: {
+				id: {
+					type: GraphQLID,
+					description: 'The movie id'
+				}	
+			},
+			resolve: (_, args) => {
+				return findVideoById(args.id)
+			}
+		}
+	}
+})
+
+const mySchema = new GraphQLSchema({
+	query: queryType,
+
+})
+
+const findVideoById = (id) => {
+	return new Promise((resolve)=>{
+		const [video] = videos.filter((video) => {
+			return video.id == id;
+		})	
+		
+		resolve(video);
+	})
 }
 
 
-// In our query we must specify properties that exist in our mySchema.Query
-const myQuery = `
-	query myFirstQuery {
-		video {
-			id,
-			title
-		},
-		videos {
-			id,
-			title,
-			duration
-		}
-	}
-`
-
-
-// graphql(mySchema, myQuery, myResolvers)
-// .then((res)=>console.log(JSON.stringify(res)))
-// .catch(err => console.log(err));
-
-
-app.use('/graphql', graphqlHTTP({
-  schema: mySchema,
-  graphiql: true,
-  rootValue: myResolvers
+app.use('/graphql', graphqlHTTP(() => {
+   const startTime = Date.now()
+   return {
+	  schema: mySchema,
+	  graphiql: true,
+	  extensions({ document, variables, operationName, result }) {
+		  return { runTime: Date.now() - startTime };
+	  }
+  }
 }));
 
 
