28b1444525c22ff822747be30b3b0035a30f24f7
Márcio Reis <marcio.reis@outlook.com>
2017-06-24 22:41:11 +0200

    add cors

--- index.js
+++ index.js
@@ -171,7 +171,21 @@ const findVideoById = (id) => {
 	})
 }
 
-
+function cors(){
+	return function (req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        if (req.method.toLowerCase() === 'options'){
+            // send answer as soon as it hits the server
+            res.status(200).end();
+        } else {
+            next()
+        }
+    }
+}
+app.use(cors())
 app.use('/graphql', graphqlHTTP(() => {
    const startTime = Date.now()
    return {
